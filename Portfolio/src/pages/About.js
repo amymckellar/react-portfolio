8c48bbf5a8a4194b4f04f211bd7a660e6e11c1c3
amymckellar <amy_mckellar@yahoo.com>
2023-04-14 20:44:16 -0500

    adding about me in the about.js file

---
+++ Portfolio/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">About Me</h1>
+      <p>
+        I am a full stack web developer with a background in the Environmental
+        Field. I have a passion for learning new things and giving myself
+        challenges. I am currently enrolled in the University of Texas at Austin
+        Full Stack Web Development Bootcamp while I work full time as a Natural
+        Resource Specialist. I am looking to transition into a career in web
+        development.
+      </p>
+    </section>
+  );
+}
