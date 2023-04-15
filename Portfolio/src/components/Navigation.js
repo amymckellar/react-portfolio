871e63f1bd12487999b1a2eded2a80e7605493fb
amymckellar <amy_mckellar@yahoo.com>
2023-04-14 21:03:29 -0500

    npm i

--- Portfolio/src/components/Navigation.js
+++ Portfolio/src/components/Navigation.js
@@ -2,19 +2,14 @@ import React from "react";
 
 function Navigation({ currentPage, handlePageChange }) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <h1>Amy McKellar </h1>
-      <header>Portfolio</header>
-      <div>
-        <a href="#about" onClick={() => handlePageChange("About")}>
+    <ul style={{ background color: "lightblue" }}>
+      <li className="nav-item">
+        <a class name="nav-link" href="#about" onClick={() => handlePageChange("About")}>
           About Me
         </a>
-        <a href="#Projects" onClick={() => handlePageChange("Portfolio")}>
-          Projects
+      </li>
+      <li className="nav-item">
+        <a class name="nav-link" href="#portfolio" onClick={() => handlePageChange("Portfolio")}>
+          Portfolio
         </a>
-      </div>
-    </nav>
-  );
-}
-
 export default Navigation;
